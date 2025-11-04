3011f695e648b06e860043280f32fd29f4713df8
minhhieunv07-glitch <minhhieu.nv07@gmail.com>
2025-11-04 20:46:09 +0700

    player

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveInput = moveInput.normalized;
+
+        if (moveInput.x < 0)
+            spriteRenderer.flipX = true;
+        else if (moveInput.x > 0)
+            spriteRenderer.flipX = false;
+
+        if (animator != null)
+            animator.SetBool("isRun", moveInput != Vector2.zero);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"Player collided with: {collision.gameObject.name} (layer {LayerMask.LayerToName(collision.gameObject.layer)})");
+    }
+}
